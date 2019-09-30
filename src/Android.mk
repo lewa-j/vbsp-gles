e5f7c01186126c8298f515a4b231032898b37c28
lewa_j <lewa.j20@gmail.com>
2019-09-30 22:05:55 +0300

    fix android build

--- src/Android.mk
+++ src/Android.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH := $(call my-dir)
-VBSP_DIR = ../../vbsp-renderer/
-ENGINE_DIR =../nenuzhno-engine/
+VBSP_DIR = ../../vbsp-gles/
+ENGINE_DIR =../../nenuzhno-engine_iter1/src/
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := nenuzhno-engine
-LOCAL_CFLAGS    := -Wall -D_VBSP
-LOCAL_C_INCLUDES := . ../libs/glm ../nenuzhno-engine
+LOCAL_CFLAGS    := -Wall -D_VBSP -std=c++11
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/. $(LOCAL_PATH)/../../libs/glm/glm $(LOCAL_PATH)/$(ENGINE_DIR)
 LOCAL_SRC_FILES := $(ENGINE_DIR)android_backend.cpp $(ENGINE_DIR)system/FileSystem.cpp $(ENGINE_DIR)system/config.cpp $(ENGINE_DIR)log.cpp $(ENGINE_DIR)game/IGame.cpp $(ENGINE_DIR)cull/frustum.cpp $(ENGINE_DIR)cull/BoundingBox.cpp \
 	$(ENGINE_DIR)graphics/gl_utils.cpp $(ENGINE_DIR)graphics/glsl_prog.cpp $(ENGINE_DIR)graphics/texture.cpp $(ENGINE_DIR)graphics/fbo.cpp $(ENGINE_DIR)graphics/vbo.cpp \
 	$(ENGINE_DIR)renderer/font.cpp $(ENGINE_DIR)renderer/camera.cpp $(ENGINE_DIR)renderer/Model.cpp \
@@ -16,8 +16,8 @@ LOCAL_SRC_FILES := $(ENGINE_DIR)android_backend.cpp $(ENGINE_DIR)system/FileSyst
 LOCAL_LDLIBS := -llog -lGLESv2 -lm -lEGL
 LOCAL_STATIC_LIBRARIES =
 
-LOCAL_C_INCLUDES += ../libs/bullet/src
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../libs/bullet3-2.86.1/src
 LOCAL_LDLIBS += -lBullet
-LOCAL_LDFLAGS += -L../libs/bullet
+LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../../libs/bullet3-2.86.1/lib
 
 include $(BUILD_SHARED_LIBRARY)
